5a5aa6f0a7105799c0c871cc7d6aa712c332be27
chchen7 <chchen.cs14@nycu.edu.tw>
2026-01-13 22:42:03 +0800

    expose docker api

--- run_exp.sh
+++ run_exp.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-for e in $(seq 2 3); do
+DOCKER_API_TCP="tcp://0.0.0.0:2375"
+if ! cat /lib/systemd/system/docker.service | grep "$DOCKER_API_TCP" 2>&1 > /dev/null; then
+
+    sed -i "/^ExecStart=/ s|$| -H $DOCKER_API_TCP|" /lib/systemd/system/docker.service
+
+    systemctl daemon-reload
+    service docker restart
+fi
+
+for e in $(seq 2 2); do
     for c in 0 1; do
         echo "Run core $c tests (exec $e)"
         for w in 500 400 300 200 100; do
