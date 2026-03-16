9a3b09c80ff601cce5e6596527d2a06ecef0b0cc
chchen7 <chchen.cs14@nycu.edu.tw>
2026-03-16 23:27:02 +0800

    update filler

--- filler_open5gs.sh
+++ filler_open5gs.sh
@@ -38,8 +38,8 @@ while (i <= $TOTAL_COUNT) {
                 "_id": ObjectId(),      
                 "pcc_rule": [],
                 "ambr": { 
-                    "uplink": { "value": NumberInt(4), "unit": NumberInt(3) }, 
-                    "downlink": { "value": NumberInt(4), "unit": NumberInt(3) } 
+                    "uplink": { "value": NumberInt(1), "unit": NumberInt(3) }, 
+                    "downlink": { "value": NumberInt(2), "unit": NumberInt(3) } 
                 },
                 "qos": { 
                     "index": NumberInt(9), 
@@ -52,8 +52,8 @@ while (i <= $TOTAL_COUNT) {
             }]
         }],
         "ambr": {
-            "uplink": { "value": NumberInt(4), "unit": NumberInt(3) },
-            "downlink": { "value": NumberInt(4), "unit": NumberInt(3) }
+            "uplink": { "value": NumberInt(1), "unit": NumberInt(3) },
+            "downlink": { "value": NumberInt(2), "unit": NumberInt(3) }
         },
         "security": {
             "k": "465B5CE8 B199B49F AA5F0A2E E238A6BC",
